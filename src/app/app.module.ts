d3e5742ebc7b4f8692354baf1f4439f3fe8c0985
Tracy Lee <tracy@wingman.events>
2017-02-09 04:49:57 -0800

    save wip

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,17 +2,31 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
+import { MaterialModule } from '@angular/material';
+import { routing } from './app.routing';
 
 import { AppComponent } from './app.component';
+import { MeowComponent } from './meow/meow.component';
+import { QuackComponent } from './quack/quack.component';
+import { MooComponent } from './moo/moo.component';
+import { BananaComponent } from './banana/banana.component';
+import { HomeComponent } from './home/home.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MeowComponent,
+    QuackComponent,
+    MooComponent,
+    BananaComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule,
     FormsModule,
-    HttpModule
+    HttpModule,
+    MaterialModule.forRoot(),
+    routing
   ],
   providers: [],
   bootstrap: [AppComponent]
